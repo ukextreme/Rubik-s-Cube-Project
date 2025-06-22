b202fd13ce5febf3b71b4c34da9a8d5f1f03be8a
ukextreme <udaykeshav2005@gmail.com>
2025-06-22 13:41:51 +0530

    some changes incorporated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Lakshya Mittal on 17-12-2021.
+// Created by Uday Keshav 
 //
 
 #include <bits/stdc++.h>
@@ -265,14 +265,15 @@ int main() {
 
 
 // CornerDBMaker Testing --------------------------------------------------------------------------
-    string fileName = "C:\\Users\\user\\CLionProjects\\rubiks-cube-solver\\Databases\\cornerDepth5V1.txt";
+    string fileName = "C:\\Users\\udayk\\OneDrive\\Desktop\\myrubik\\myproject\\Databases\\cornerDepth5V1.txt";
+
 
 //    Code to create Corner Database
 //    CornerDBMaker dbMaker(fileName, 0x99);
 //    dbMaker.bfsAndStore();
 
     RubiksCubeBitboard cube;
-    auto shuffleMoves = cube.randomShuffleCube(13);
+    auto shuffleMoves = cube.randomShuffleCube(3);
     cube.print();
     for (auto move: shuffleMoves) cout << cube.getMove(move) << " ";
     cout << "\n";
